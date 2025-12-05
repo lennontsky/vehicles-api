a7e7b478ee4cc5d967ffba7a4cfe3f5c2501e997
lennontsky <clara.rosa.miranda@gmail.com>
2025-12-05 03:00:42 -0300

    :sparkles: polimento swagger

--- src/main/java/br/com/cannamiranda/vehicles_api/VehiclesApiApplication.java
+++ src/main/java/br/com/cannamiranda/vehicles_api/VehiclesApiApplication.java
@@ -1,13 +1,18 @@
 package br.com.cannamiranda.vehicles_api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "Vehicles API",
+        version = "0.1",
+        description = "API para teste técnico da Tinnova")
+)
 public class VehiclesApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(VehiclesApiApplication.class, args);
 	}
-
 }
